2d04400c0e9381ebdccb4f359300e2dd43bc54d2
Karthi3754 <karthis3754@gmail.com>
2024-12-25 21:44:01 +0530

    first commit

---
+++ sort.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h> // For qsort
+
+// Comparison function for ascending order
+int compare(const void* a, const void* b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int arr[n];
+    
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    // Sort the array in ascending order
+    qsort(arr, n, sizeof(int), compare);
+
+    // Print the sorted array
+    for (int i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+
+    return 0;
+}
+
+//Time complexity : 0(n log(n))
+//space complexity : 0(log(n))
